4058583797b09ad2dd3bc2b2bdf9c7b52e93b7e3
Vladyslav Plaksa <vladyslavplaksa@gmail.com>
2023-09-27 13:26:17 +0200

    add api/users route

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-unresolved */
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        const prompts = await Prompt.find({ creator: params.id }).populate('creator');
+
+        return new Response(JSON.stringify(prompts), { status: 200 });
+    } catch {
+        return new Response('Failed to fetch prompts created by user', { status: 500 });
+    }
+};
