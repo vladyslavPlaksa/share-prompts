e93c19ad243358e449be2287c2fe9e1bc821dd64
Vladyslav Plaksa <vladyslavplaksa@gmail.com>
2023-09-27 13:25:51 +0200

    add api/prompt route

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-unresolved */
+import Prompt from '@models/prompt';
+import { connectToDB } from '@utils/database';
+
+export const POST = async request => {
+    const { userId, prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+        const newPrompt = new Prompt({ creator: userId, prompt, tag });
+
+        await newPrompt.save();
+        return new Response(JSON.stringify(newPrompt), { status: 201 });
+    } catch {
+        return new Response('Failed to create a new prompt', { status: 500 });
+    }
+};
